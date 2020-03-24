4ace64e9019f022ae9740db4bdba13b552606431
ElijahMcKay <elijahmckay10@gmail.com>
2020-03-24 16:34:17 -0500

    feat: added HLT and unknown cases to run function in CPU class

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,77 +2,86 @@
 
 import sys
 
+
 class CPU:
-    """Main CPU class."""
+	"""Main CPU class."""
 
-    def __init__(self, ram, reg, pc):
-        """Construct a new CPU."""
-        self.ram = ram
-        self.reg = reg
-        self.pc = pc
+	def __init__(self, ram, reg, pc):
+	"""Construct a new CPU."""
+	self.ram = ram
+	self.reg = reg
+	self.pc = pc
 
-    def ram_read(self, pc):
-        print(pc)
+	def ram_read(self, pc):
+	print(pc)
 
-    def ram_write(self):
-        pass
+	def ram_write(self):
+	pass
 
-    def load(self):
-        """Load a program into memory."""
+	def load(self):
+		"""Load a program into memory."""
 
-        address = 0
+		address = 0
 
-        # For now, we've just hardcoded a program:
+		# For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+		program = [
+				# From print8.ls8
+						0b10000010,  # LDI R0,8
+						0b00000000,
+						0b00001000,
+						0b01000111,  # PRN R0
+						0b00000000,
+						0b00000001,  # HLT
+			]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+	for instruction in program:
+		self.ram[address] = instruction
+		address += 1
 
+	def alu(self, op, reg_a, reg_b):
+		"""ALU operations."""
 
-    def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
+		if op == "ADD":
+			self.reg[reg_a] += self.reg[reg_b]
+		elif op == "SUB":
+			self.reg[reg_a] -= self.reg[reg_b]
+		elif op == "MUL":
+			self.reg[reg_a] *= self.reg[reg_b]
+		elif op == "DIV":
+			self.reg[reg_a] /= self.reg[reg_b]
+		else:
+			raise Exception("Unsupported ALU operation")
 
-        if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        elif op == "SUB":
-            self.reg[reg_a] -= self.reg[reg_b]
-        elif op == "MUL":
-            self.reg[reg_a] *= self.reg[reg_b]
-        elif op == "DIV":
-            self.reg[reg_a] /= self.reg[reg_b]
-        else:
-            raise Exception("Unsupported ALU operation")
+	def trace(self):
+		"""
+		Handy function to print out the CPU state. You might want to call this
+		from run() if you need help debugging.
+		"""
 
-    def trace(self):
-        """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
+		print(f"TRACE: %02X | %02X %02X %02X |" % (
+				self.pc,
+				#self.fl,
+				#self.ie,
+				self.ram_read(self.pc),
+				self.ram_read(self.pc + 1),
+				self.ram_read(self.pc + 2)
+			), end='')
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
+		for i in range(8):
+			print(" %02X" % self.reg[i], end='')
 
-        for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+			print()
 
-        print()
+	def run(self, pc):
+		"""Run the CPU."""
+		# instruction equals current index of program counter "pc"
+		instruction = self.pc
+		halted = False
 
-    def run(self):
-        """Run the CPU."""
-        pass
+		while not halted:
+			if instruction == "HLT":
+				halted = True
+			else:
+				print(f'Unknown instructions at index {self.pc}')
+				sys.exit(1)
